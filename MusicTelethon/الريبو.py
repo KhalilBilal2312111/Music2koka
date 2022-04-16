07d5bd165b3a44b75c4c64a61048fa9078467f36
KhalilBilal2312111 <103786205+KhalilBilal2312111@users.noreply.github.com>
2022-04-16 04:59:05 +0300

    Update الريبو.py

--- MusicTelethon/الريبو.py
+++ MusicTelethon/الريبو.py
@@ -57,8 +57,8 @@ async def help(client, m: Message):
 
 ⧉ | لأعاده تشغيل التنصيب أرسل ⇦  [ `{HNDLR}ريستارت` ]
 ———————×———————
-🛠 | @TelethonMusic
-⭐ | @LLL5L"""
+♥ | @TFG_FQ
+⭐ | @Ti_iT01"""
     await m.reply(HELP)
 @Client.on_message(filters.command(["الريبو"], prefixes=f"{HNDLR}"))
 async def repo(client, m: Message):
@@ -66,7 +66,7 @@ async def repo(client, m: Message):
     REPO = f"""
 <b>👋  اهلا {m.from_user.mention}!
 
-🎶 هذا ميوزك العرب | @TelethonMusic
-⭐ المطور | @LLL5L
+♥ قناة تلي | @FG_FQ
+⭐ المطور | @Ti_iT01
 """
     await m.reply(REPO, disable_web_page_preview=True)
